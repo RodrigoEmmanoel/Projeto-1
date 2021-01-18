78a6b82a6a25116189d1488e05fa81919fabe643
RodrigoEmmanoel <rodrigoemmanoel98@gmail.com>
2021-01-18 20:12:11 -0300

    function main for update table

--- index.js
+++ index.js
@@ -26,13 +26,49 @@ const accessSheet = async () => {
     // rows.forEach(row => {
     //         await promisify(worksheet.addColumn)({ _ckd7g = 10 })    
     // })
-    await promisify(worksheet.addColumns)({
-        engenhariadesoftware : 25
-    })
+    // await promisify(worksheet.addColumns)({
+    //     engenhariadesoftware : 25
+    // })
 
     // await promisify(worksheet.values.update(('engenhariadesoftware', '25')))
 
+    async function main() {
+        const authClient = await authorize();
+        const request = {
+            // The ID of the spreadsheet to update.
+            spreadsheetId: 'engenharia-de-software-teste',  // TODO: Update placeholder value.
 
+            // The A1 notation of the values to update.
+            range: 'engenhariadesoftware',  // TODO: Update placeholder value.
+
+            // How the input data should be interpreted.
+            engenhariadesoftware: '25',  // TODO: Update placeholder value.
+
+            resource: {
+                // TODO: Add desired properties to the request body. All existing properties
+                engenhariadesoftware: '25',
+                _cokwr: 'Aluno',
+                _cpzh4: 'Faltas',
+                _cre1l: 'P1',
+                _chk2m: 'P2',
+                _ciyn3: 'P3',
+                _ckd7g: 'Situação',
+
+                // will be replaced.
+            },
+
+            auth: authClient,
+        };
+        try {
+            const response = (await sheets.spreadsheets.values.update(request)).data;
+            // TODO: Change code below to process the `response` object:
+            console.log(JSON.stringify(response, null, 2));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    main()
 }
 
 accessSheet()
